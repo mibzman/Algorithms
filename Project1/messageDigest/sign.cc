8083246f675e3319a5f0bf8952b41cb0c28c09f1
mibzmam <27thisotope@gmail.com>
2017-02-10 13:44:43 -0500

    finished signing

--- Project1/messageDigest/sign.cc
+++ Project1/messageDigest/sign.cc
@@ -2,123 +2,74 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> 
+#include <time.h>
 
 
 #include "BigIntegerLibrary.hh"
 #include "BigIntegerAlgorithms.hh"
+#include "messageDigest/sha256.h"
 //another student did somehting like this, I thought it was a great idea
 #define bAlgos BIGINTEGERALGORITHMS_H
+#define bUtils BIGINTEGERUTILS_H
+#define sha SHA256_H
 
-//I kept all this stuff because I didn't want you to have to run part 1 as a dependancy.
-//nothing is changed in here
-bool Fermat(BigInteger& p, int lim){
-	int counter = 1;
-	while (counter != lim){
-		const BigInteger start = BigInteger(rand()) % (p - 1) + 1;
-		BigUnsigned val = bAlgos::modexp(start, p.getMagnitude()-1, p.getMagnitude());
-		if(val != 1){
-			return false;
-		}
-		++counter;
+std::string readData(std::ifstream& reader){
+	std::stringstream buffer;
+	buffer << reader.rdbuf();
+	return buffer.str();
+}
+
+void Sign(BigInteger* d, BigInteger* n, std::string file){
+	std::ifstream reader(file);
+	if (reader){
+		std::string data = readData(reader);
+		fileReader.close();
+		std::string hash = sha::sha256(data);
+		BigInteger temp(sha_hash.size());
+		BigInteger m = bUtils::dataToBigInteger<const char>(hash.c_str(), temp.getLength(), temp.getSign());
+		const BigInteger decrypt = bAlgo::modexp(m, d.getMagnitude(), n.getMagnitude());
+		file += ".signed";
+		std::ofstream signedFile(file);
+		signedFile << original_data;
+		signedFile << "=====BEGIN SIGNED CONTENT=====";
+		signedFile << decrypt;
 	}
-	return true;
-}
-
-BigInteger MakeBigInt(){
-	BigInteger random = BigInteger(1);
-	for (int i=1; i<155; ++i){
-		int randDig = rand() % 10;
-        	random = random * 10 + randDig;
-         }
-	return random;
-
-}
-
-BigInteger FindPrime(){
-	BigInteger prime = MakeBigInt();
-	if (prime % 2 == 0){
-		prime++;
-	}
-	while(!Fermat(prime, 3)){
-		 prime = MakeBigInt();
-	        if (prime % 2 == 0){
-        	        prime++;
-        	}
-	}
-	return prime;
-}
-
-bool relativelyPrime(BigInteger& phi, BigInteger& maybeE){
-   return bAlgos::gcd(phi.getMagnitude(), maybeE.getMagnitude()) == 1;
-}
-
-BigInteger FindE(BigInteger phi){
-	BigInteger maybeE = BigInteger(2 * (rand() + 1) + 1);
-	while(!relativelyPrime(phi, maybeE)){
-		maybeE = BigInteger(2 * (rand() + 1) + 1);
-	}
-	return maybeE;
-}
-
-void SaveKeys(BigInteger& p, BigInteger& q, BigInteger& e, BigInteger& n, BigInteger& d){
-   std::ofstream keys;
-   keys.open("p_q.txt");
-   keys << p << std::endl << q;
-   keys.close();
-   keys.open("e_n.txt");
-   keys << e << std::endl << n;
-   keys.close();
-   keys.open("d_n.txt");
-   keys << d << std::endl << n;
-   keys.close();
-}
-
-std::string ReadString(char arr[]){
-	std::String output = "";
-	int size = strlen(arr);
-	int counter = 0;
-	while (counter < size){
-		output += *(arr + counter);
-		counter++;
-	}
-	return output;
-}
-
-
-void Sign(std::string file){
-
-//it was way more work than it was worth to break this all out into a seperate method
-	std::cout << "Imagine gears turning, wheels spinning, and steam billowing.  Big numbers are being crunched, it takes a second or two!  Sit back, relax, and enjoy." << std::endl;
-        BigInteger p = FindPrime();
-        std::cout << "p: " << p << std::endl;
-        BigInteger q = FindPrime();
-        std::cout << "q: " << q << std::endl;
-        BigInteger n = p * q;
-        std::cout << "n: " << n << std::endl;
-        BigInteger phi = (p -1) * (q - 1);
-        std::cout << "phi: " << phi << std::endl;
-        BigInteger e = FindE(phi);
-        if (e < 0) {
-	        e = e + phi;
-         }
-        std::cout << "e: " << e << std::endl;
-        BigInteger d = bAlgos::modinv(e.getMagnitude(), phi.getMagnitude());
-        std::cout << "d: " << d << std::endl;
-
 }
 
 void Verify(std::string file){
 
 }
 
-int main(int argn, char *args[]) {
-	/* The library throws `const char *' error messages when things go
-	 * wrong.  It's a good idea to catch them using a `try' block like this
-	 * one.  Your C++ compiler might need a command-line option to compile
-	 * code that uses exceptions. */
+void GetEAndN(BigInteger* e, BigInteger* n){
+	std::ifstream reader();
+	if (reader){
+		reader.open("e_n.txt");
+		if (reader){
+			std::string eString, nString;
+			reader >> sString, >> nString;
+			*e = bUtils::stringToBigInteger(eString);
+			*n = bUtils::stringToBigInteger(nString);
+		}
+	}
+	return;
+}
 
-    srand (time(NULL));
+void GetDAndN(BigInteger* d, BigInteger* n){
+	std::ifstream reader();
+	if (reader){
+		reader.open("d_n.txt");
+		if (reader){
+			std::string dString, nString;
+			reader >> sString, >> nString;
+			*d = bUtils::stringToBigInteger(dString);
+			*n = bUtils::stringToBigInteger(nString);
+		}
+	}
+	return;
+}
+
+int main(int argn, char *args[]) {
+	srand (time(NULL));
 
 	//it's super stupid that the exe is an argument
 	assert("Bad arguments.  Flags should be wither v or s" && argn == 3);
@@ -126,31 +77,22 @@ int main(int argn, char *args[]) {
 	std::string file = ReadString(args[3]);
 
 	try{
-		std::cout << "Imagine gears turning, wheels spinning, and steam billowing.  Big numbers are being crunched, it takes a second or two!  Sit back, relax, and enjoy." << std::endl;
-		BigInteger p = FindPrime();
-		std::cout << "p: " << p << std::endl; 
-		BigInteger q = FindPrime();
-		std::cout << "q: " << q << std::endl;
-		BigInteger n = p * q;
-		std::cout << "n: " << n << std::endl;
-		BigInteger phi = (p -1) * (q - 1);
-		std::cout << "phi: " << phi << std::endl;
-		BigInteger e = FindE(phi);
-		if (e < 0) {
-			e = e + phi;
+		if (*args[1] == 'v'){
+		BigInteger e,n;
+		GetEAndN(&e,&n);
+		Verify(e,n,file);
+		} else if (*args[1] == 's'){
+			BigInteger d,n;
+			GetDAndN(&d,&n);
+			Sign(d,n,file);
+		} else{
+			std::cout << "wrong flags.  Please enter either v or s followed by the file" << std::endl;
+			return 1;
 		}
-		std::cout << "e: " << e << std::endl;
-		BigInteger d = bAlgos::modinv(e.getMagnitude(), phi.getMagnitude());
-		std::cout << "d: " << d << std::endl;
-
-		SaveKeys(p,q,e,n,d);
-      
 	} catch(char const* err) {
 		std::cout << "The library threw an exception:\n"
 			<< err << std::endl;
 	}
-	//std::cout << "calculating prime" << std::endl;
-	//std::cout << FindPrime() << std::endl;
 
 	return 0;
 }
