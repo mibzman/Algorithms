3df8d36c93ffbb5310beba524fdc1afffd5ff033
mibzman <27thisotope@gmail.com>
2017-02-21 18:03:05 -0500

    changed storage to 9 bits

--- Project2/lzw435.cpp
+++ Project2/lzw435.cpp
@@ -5,6 +5,9 @@
 #include <iterator>
 #include <vector> 
 #include <sys/stat.h>
+#include <cassert>
+
+#include <sstream>
 
 /*
   This code is derived for UA CS435 from LZW@RosettaCode
@@ -13,9 +16,16 @@
 // Compress a string to a list of output symbols.
 // The result will be written to the output iterator
 // starting at "result"; the final iterator is returned.
+
+std::string readData(std::ifstream& reader){
+	std::stringstream buffer;
+	buffer << reader.rdbuf();
+	return buffer.str();
+}
+
 template <typename Iterator>
-Iterator compress(const std::string &file, Iterator result) {
-  std::string& uncompressed;
+Iterator compress(std::string file, Iterator result) {
+  std::string uncompressed;
   std::ifstream reader(file);
   uncompressed = readData(reader);
   reader.close();
@@ -116,15 +126,100 @@ int binaryString2Int(std::string p) {
    return code;
 }
 
-std::vector<int> compressed readBinary(std::string file) {   
+bool binaryTest(std::vector<int> compressed) {
+   int c = 69;
+   //int bits = 9;
+   std::string p = int2BinaryString(c, 9);
+   
+   std::string bcode= "";
+   for (std::vector<int>::iterator it = compressed.begin() ; it != compressed.end(); ++it) {
+      //if (*it<256)
+        // bits = 8;
+      //else
+        // bits = 9;
+      p = int2BinaryString(*it, 9);
+      bcode+=p;
+   }
+   
+   //writing to file
+   std::string fileName = "example435.lzw";
+   std::ofstream myfile;
+   myfile.open(fileName.c_str(),  std::ios::binary);
+   
+   std::string zeros = "00000000";
+   if (bcode.size()%8!=0) //make sure the length of the binary string is a multiple of 8
+      bcode += zeros.substr(0, 8-bcode.size()%8);
+   
+   int b; 
+   for (int i = 0; i < bcode.size(); i+=8) { 
+      b = 1;
+      for (int j = 0; j < 8; j++) {
+         b = b<<1;
+         if (bcode.at(i+j) == '1')
+           b+=1;
+      }
+      char c = (char) (b & 255); //save the string byte by byte
+      myfile.write(&c, 1);  
+   }
+   myfile.close();
+   
+   //reading from a file
+   std::ifstream myfile2;
+   myfile2.open (fileName.c_str(),  std::ios::binary);
+   
+   struct stat filestatus;
+   stat(fileName.c_str(), &filestatus );
+   long fsize = filestatus.st_size; //get the size of the file in bytes
+   
+   char c2[fsize];
+   myfile2.read(c2, fsize);
+   
+   std::string s = "";
+   long count = 0;
+   while(count<fsize) {
+      unsigned char uc =  (unsigned char) c2[count];
+      std::string p = ""; //a binary string
+      for (int j=0; j<8 && uc>0; j++) {         
+		   if (uc%2==0)
+            p="0"+p;
+         else
+            p="1"+p;
+         uc=uc>>1;   
+      }
+      p = zeros.substr(0, 8-p.size()) + p; //pad 0s to left if needed
+      s+= p; 
+      count++;
+   } 
+   myfile2.close();
+
+  std::vector<int> output;
+  int counter = 0;
+  while (counter <= s.length()){
+    output.push_back(binaryString2Int(s.substr(counter, 9)));
+    counter += 9;
+  }
+  std::cout << "compressed: "; 
+  copy(compressed.begin(), compressed.end(), std::ostream_iterator<int>(std::cout, ", "));
+  std::cout << std::endl << "----output: ";
+  copy(output.begin(), output.end(), std::ostream_iterator<int>(std::cout, ", "));
+  std::cout << std::endl;
+
+
+  for (int counter2 = 0; counter2 < compressed.size(); counter2++){
+    std::cout << "does " << compressed[counter2] << " == " << output[counter2] << std::endl;
+  }
+  output.pop_back();
+  return compressed == output;
+}
+
+std::vector<int> readBinary(std::string file) {   
   //reading from a file
   std::ifstream myfile2;
   myfile2.open (file.c_str(),  std::ios::binary);
-
   struct stat filestatus;
   stat(file.c_str(), &filestatus );
   long fsize = filestatus.st_size; //get the size of the file in bytes
-
+  std::string zeros = "00000000";
   char c2[fsize];
   myfile2.read(c2, fsize);
 
@@ -151,6 +246,7 @@ std::vector<int> compressed readBinary(std::string file) {
   int counter = 0;
   while (counter <= s.length()){
     output.push_back(binaryString2Int(s.substr(counter, counter+9)));
+    counter += 8;
   }
    return output;
 }
@@ -201,15 +297,20 @@ int main(int argn, char *args[]) {
 
   std::string file(args[2]);
 
+  //std::cout << binaryTest
+
   std::vector<int> compressed;
 
   if (*args[1] == 'c'){
     compress(file, std::back_inserter(compressed));
+    //std::cout << binaryTest(compressed) << std::endl;
+    //copy(compressed.begin(), compressed.end(), std::ostream_iterator<int>(std::cout, ", "));
     printBinary(file + ".zip", compressed);
   } else if (*args[1] == 'd'){
     compressed = readBinary(file);
-    std::string decompressed = decompress(compressed.begin(), compressed.end());
-    std::cout << decompressed << std::endl;
+    copy(compressed.begin(), compressed.end(), std::ostream_iterator<int>(std::cout, ", "));
+    //std::string decompressed = decompress(compressed.begin(), compressed.end());
+    //std::cout << decompressed << std::endl;
   } else{
     std::cout << "wrong flags.  Please enter either c or d followed by the file" << std::endl;
     return 1;
