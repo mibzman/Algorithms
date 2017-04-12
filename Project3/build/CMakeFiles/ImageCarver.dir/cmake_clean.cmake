a82e3e9f6e996e0b9895edd35e89d3a5f9c41ac5
Sam Borick <mibzman@27thisotope.ddns.net>
2017-04-12 09:03:37 -0400

    everything

---
+++ Project3/build/CMakeFiles/ImageCarver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageCarver.dir/SeamCarveImage.cpp.o"
+  "ImageCarver.pdb"
+  "ImageCarver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImageCarver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
